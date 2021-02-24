7cb2313c4b5d371b9b80e7ae86f35da01113dc11
atrox3d <atrox3d@gmail.com>
2021-02-24 07:29:45 +0100

    fix UI

--- ui.py
+++ ui.py
@@ -10,34 +10,34 @@ class QuizUI:
         self.window = tkinter.Tk()
         self.window.title = "Quizzler"
         self.window.config(
-            bg=THEME_COLOR,  # window background color
-            padx=20,  # window border x
-            pady=20  # windows border y
+            bg=THEME_COLOR,     # window background color
+            padx=20,            # window border x
+            pady=20             # windows border y
         )
 
         self.score = tkinter.Label(
-            text="Score",  # label text
-            bg=THEME_COLOR,  # label background color
-            fg=WHITE  # label text color
+            text="Score: 0",    # label text
+            bg=THEME_COLOR,     # label background color
+            fg=WHITE            # label text color
         )
         self.score.grid(row=0, column=1)
 
-        self.canvas = tkinter.Canvas(width=300, height=250)
-        self.canvas.grid(row=1, column=0, columnspan=2)
+        self.canvas = tkinter.Canvas(width=300, height=250, bg=WHITE)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         self.question = self.canvas.create_text(
-            150,  # text center x
-            125,  # text center y
+            150,                # text center x
+            125,                # text center y
             text="question",
             font=QUESTION_FONT
         )
 
-        self.true_image = tkinter.PhotoImage(file="images/true.png")
-        self.true_button = tkinter.Button(image=self.true_image)
+        true_image = tkinter.PhotoImage(file="images/true.png")
+        self.true_button = tkinter.Button(image=true_image)
         self.true_button.grid(row=2, column=0)
 
-        self.false_image = tkinter.PhotoImage(file="images/false.png")
-        self.false_button = tkinter.Button(image=self.false_image)
+        false_image = tkinter.PhotoImage(file="images/false.png")
+        self.false_button = tkinter.Button(image=false_image)
         self.false_button.grid(row=2, column=1)
 
         self.window.mainloop()

4f66f416535bcf84f553d1f6ded6c7fe8afb7aed
atrox3d <atrox3d@gmail.com>
2021-02-24 06:48:22 +0100

    add class QuizUI

--- ui.py
+++ ui.py
@@ -1,2 +1,10 @@
+import tkinter
 THEME_COLOR = "#375362"
 
+
+class QuizUI:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.window.title = "Quizzler"
+
+        self.window.mainloop()
